ec0555769280cff0306146584455efab54446eef
ronaldoxor <rronaldo.monteiro.almeida@gmail.com>
2026-09-15 08:53:21 -0300

    Comentando uma parte

--- src/main/java/com/fiap/bank/atm/domain/repository/AccountRepository.java
+++ src/main/java/com/fiap/bank/atm/domain/repository/AccountRepository.java
@@ -3,6 +3,8 @@ package com.fiap.bank.atm.domain.repository;
 import com.fiap.bank.atm.domain.model.Account;
 import java.util.Optional;
 
+// Interface
+
 public interface AccountRepository {
     Optional<Account> findByAccountNumber(String accountNumber);
     void save(Account account);
